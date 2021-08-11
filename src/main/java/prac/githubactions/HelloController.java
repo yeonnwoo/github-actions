58530f132cd4b41ec0211f3b1002b37065dc071d
Yeonwoo-Kim <yeonkim0726@gmail.com>
2021-08-11 23:42:25 +0900

    Initial commit

---
+++ src/main/java/prac/githubactions/HelloController.java
@@ -0,0 +1,14 @@
+package prac.githubactions;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String Hello()
+    {
+        return "hi";
+    }
+}
